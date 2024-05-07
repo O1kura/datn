99fc3752d44f1d0174df80587c66fad258e2e499
O1kura <viet6112001@gmail.com>
2024-05-08 04:16:35 +0700

    throttle for anon user

--- datn/settings.py
+++ datn/settings.py
@@ -152,7 +152,14 @@ REST_FRAMEWORK = {
                                 'rest_framework.filters.SearchFilter'],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer'
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        # 'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '24/day'
+    }
 }
 
 SIMPLE_JWT = {
